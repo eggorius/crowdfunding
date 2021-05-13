64b76b86bc475c451255e0065e825ff22c70c875
egorius <petrenkoeggg@gmail.com>
2021-05-13 22:06:25 +0300

    Updated settings

--- Finals/settings.py
+++ Finals/settings.py
@@ -10,12 +10,12 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-pxeu^rnv01mo-*oiwdwl(h1a7tl=&sc=f@2s@d&ecj=a!$ydxu'
+SECRET_KEY = os.environ.get('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['https://crowdfundingcompanies.herokuapp.com/']
 
 
 # Application definition
