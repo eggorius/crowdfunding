9f28d71917f3509d04cad199aecbec24a77f7d80
egorius <petrenkoeggg@gmail.com>
2021-05-15 15:56:18 +0300

    Before adding cloundinary

--- main/static/js/company_detail.js
+++ main/static/js/company_detail.js
@@ -2,11 +2,12 @@ const rating = document.querySelector('form[name=rating]');
 
 rating.addEventListener("change", function (e) {
     // Getting data from the form
-    let data = new FormData(this);
+    const data = new FormData(this)
+    console.log(data)
     fetch(`${this.action}`, {
         method: 'POST',
         body: data
     })
-        .then(response => alert("Rating saved"))
-        .catch(error => alert("Error"))
+      .then(response => alert("Rating saved"))
+      .catch(error => alert("Error"))
 });
