6b1e9edf4f21f0a47c515c645d752f387cfab327
egorius <petrenkoeggg@gmail.com>
2021-05-17 12:33:08 +0300

    Added donation sweetalert

--- main/static/js/company_detail.js
+++ main/static/js/company_detail.js
@@ -1,15 +1,36 @@
 const rating = document.querySelector('form[name=rating]');
 
 rating.addEventListener("change", function (e) {
-    // Getting data from the form
-    const data = new FormData(this)
-    console.log(data)
-    fetch(`${this.action}`, {
-        method: 'POST',
-        body: data
-    })
-      .then(response => alert("Rating saved"))
-      .catch(error => alert("Error"))
+  // Getting data from the form
+  const data = new FormData(this)
+  console.log(data)
+  fetch(`${this.action}`, {
+    method: 'POST',
+    body: data
+  })
+    .then(response => alert("Rating saved"))
+    .catch(error => alert("Error"))
 });
+
+button = document.querySelector('button[name=donate]')
+button.addEventListener('click', e => {
+  Swal.fire({
+    title: 'Enter how much do you want to donate',
+    input: 'text',
+    showCancelButton: true,
+    confirmButtonText: 'Donate',
+    showLoaderOnConfirm: true,
+  }).then((result) => {
+    if (result.isConfirmed) {
+      Swal.fire(
+        'Thank you!',
+        'We appreciate your support.',
+        'success'
+      )
+    }
+  })
+})
+
+
 
 
